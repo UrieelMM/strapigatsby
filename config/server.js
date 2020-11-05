5139a93e462907fd7c341ad41080333ae29667b9
Urieel Mejia <uriel.mejia.unitec@outlook.com>
2020-11-05 12:52:51 -0600

    Second Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f867c7382e9547bf98b6a5fdf1672253'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17e99d52-01ab-4795-865c-2b341523d2bb'
+};
